f07e7bf6b6a204468ba23eaf411279753f33709f
Jason <JPriskomatis@gmail.com>
2024-06-16 19:19:13 +0300

    teleportation

---
+++ Assets/Scripts/Portals/Portal.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject teleportDestination; // Destination GameObject where player will teleport
+    public GameObject teleportTrigger; // Trigger GameObject to detect player's touch
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the player (or any object with the "Player" tag) touches the teleport trigger
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player");
+            // Teleport the player to the destination
+            TeleportPlayer(other.gameObject);
+        }
+    }
+
+    private void TeleportPlayer(GameObject player)
+    {
+        // Ensure there's a destination set
+        if (teleportDestination != null)
+        {
+            // Teleport the player to the destination's position    
+            player.transform.position = teleportDestination.transform.position;
+            Debug.Log("Player teleported to: " + teleportDestination.name);
+        }
+        else
+        {
+            Debug.LogWarning("Teleport destination is not set!");
+        }
+    }
+}
